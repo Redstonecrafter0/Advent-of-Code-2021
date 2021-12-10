b5f956b6a67f5c437cc6d2e618f5640a18a068c6
Redstonecrafter0 <54239558+Redstonecrafter0@users.noreply.github.com>
2021-12-10 11:00:04 +0100

    Day 8 part 2

--- src/Day08.kt
+++ src/Day08.kt
@@ -10,12 +10,29 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf {
+            val sizes = it.split(" | ").map { s -> s.split(" ").map { d -> d.toSet() } }.flatten().groupBy { s -> s.size }
+
+            val nums = Array(10) { setOf<Char>() }
+
+            nums[1] = sizes[2]!!.first()
+            nums[4] = sizes[4]!!.first()
+            nums[7] = sizes[3]!!.first()
+            nums[8] = sizes[7]!!.first()
+            nums[6] = sizes[6]!!.first { s -> nums[8] == nums[1] + s }
+            nums[5] = sizes[5]!!.first { s -> nums[8] != nums[6] + s }
+            nums[2] = sizes[5]!!.first { s -> nums[8] == nums[5] + s }
+            nums[9] = sizes[6]!!.first { s -> nums[8] != nums[4] + s }
+            nums[0] = sizes[6]!!.first { s -> nums[8] == nums[5] + s }
+            nums[3] = sizes[5]!!.first { s -> s !in nums }
+
+            it.split(" | ")[1].split(" ").map { s -> nums.indexOf(s.toSet()) }.joinToString("").toInt()
+        }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day08_test")
-    check(part1(testInput) == 26)
+    check(part2(testInput) == 61229)
 
     val input = readInput("Day08")
     println(part1(input))
